922298c9e941f05d3d3cd7c0b33985fcb3238480
Walter V. Faustine <waltervfaustine@gmail.com>
2019-06-05 17:38:54 +0300

    feat(filters): added support forvalidation rule group filter

--- projects/ngx-dhis2-selection-filters/src/lib/ngx-dhis2-selection-filters.module.ts
+++ projects/ngx-dhis2-selection-filters/src/lib/ngx-dhis2-selection-filters.module.ts
@@ -7,18 +7,24 @@ import { NgxDhis2DataFilterModule } from '@iapps/ngx-dhis2-data-filter';
 import { NgxDhis2PeriodFilterModule } from '@iapps/ngx-dhis2-period-filter';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxDhis2ValidationRuleFilterModule } from '@iapps/ngx-dhis2-validation-rule-group-filter';
 
 @NgModule({
   imports: [
     CommonModule,
     TranslateModule.forChild(),
+    NgxDhis2ValidationRuleFilterModule,
     NgxDhis2OrgUnitFilterModule,
     NgxDhis2DataFilterModule,
     NgxDhis2PeriodFilterModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    HttpClientModule
   ],
   declarations: [NgxDhis2SelectionFiltersComponent],
-  exports: [NgxDhis2SelectionFiltersComponent]
+  exports: [NgxDhis2SelectionFiltersComponent],
+  providers: []
 })
 export class NgxDhis2SelectionFiltersModule {}
+
