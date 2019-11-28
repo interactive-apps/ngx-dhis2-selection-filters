39b326b1a138c9cd8d166ed9fb86094494522757
chingaloEric <echingalo@gmail.com>
2019-11-28 09:07:47 +0300

    style(app): replaced double quotes with single quotes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,21 @@
-import { Component } from "@angular/core";
-import { SelectionFilterConfig } from "projects/ngx-dhis2-selection-filters/src/public_api";
+import { Component } from '@angular/core';
+import { SelectionFilterConfig } from 'projects/ngx-dhis2-selection-filters/src/public_api';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = "app";
+  title = 'app';
   selectionFilterConfig: SelectionFilterConfig = {
     allowStepSelection: true,
     disablePeriodTypeSelection: true,
-    selectedPeriodType: "Monthly",
-    stepSelections: ["dx", "vrg", "pe", "ou"],
+    selectedPeriodType: 'Monthly',
+    stepSelections: ['dx', 'vrg', 'pe', 'ou'],
     dataFilterConfig: {
       singleSelection: true,
-      enabledSelections: ["in"]
+      enabledSelections: ['in']
     },
     periodFilterConfig: {
       singleSelection: true
